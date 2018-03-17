2d365c0d3cece7812886056cf7d60c7f4bdf4550
jmddesarrollo <jmddesarrollo@gmail.com>
2018-03-17 17:39:57 +0100

    FrontEnd: Control de sesión del usuario

--- 1_Sources/client-spotify/src/app/app.component.ts
+++ 1_Sources/client-spotify/src/app/app.component.ts
@@ -1,10 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Importar modelos
+import { Usuario } from './models/usuario';
+import { UsuarioService } from './services/usuario.services';
+import { NgLocaleLocalization } from '@angular/common/src/i18n/localization';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsuarioService]	
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public titulo = 'MusicaFy';
+  public usuario: Usuario;
+  // Contendrá todo el objeto del usuario logeado. En localStorage guardamos el objeto identity, y al iniciar cualquier petición recogeremos esa información del localStorage.
+  public identity;
+  // Guardado en localStorage. Se utiliza este objeto para pasarselo al servicio.
+  public token;
+  public errorMensaje: string;
+
+  constructor(
+    private _usuarioService: UsuarioService
+  ) {
+    this.usuario = new Usuario(null, '', '', '', '', '', '');
+  }
+
+  ngOnInit() {
+    this.identity = this._usuarioService.getIdentity();
+    this.token = this._usuarioService.getToken();
+  }
+
+  onSubmit() {
+    // Conseguir los datos de usuario.
+    this._usuarioService.signup(this.usuario).subscribe(
+      response => {
+        let identity = response.usuario;
+        this.identity = identity;
+
+        if (!this.identity.id) {
+          alert('El usuario no está identificado correctamente.');
+        } else {
+          // Crear elemento en el localStorage para tener al usuario en sesión
+          localStorage.setItem('identity', JSON.stringify(identity));
+
+          // Conseguir el token para enviarselo a cada petición
+          this._usuarioService.signup(this.usuario, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+
+              if (this.token.lenght <= 0) {
+                alert('El token no se ha generado correctamente.');
+              } else {
+                localStorage.setItem('token', token);
+              }
+            },
+            error => {
+              this.errorMensaje = <any>error;
+
+              if (this.errorMensaje) {
+                var body = JSON.parse(error._body);
+                this.errorMensaje = body.mensaje;
+              }
+            }
+          );
+        }
+      },
+      error => {
+        this.errorMensaje = <any>error;
+
+        if (this.errorMensaje) {
+          var body = JSON.parse(error._body);
+          this.errorMensaje = body.mensaje;
+        }
+      }
+    );
+  }
+
+  ocultarError() {
+    this.errorMensaje = null;
+  }
+
+  logout() {
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+
+    this.identity = null;
+    this.token = null;
+  }
 }
---
+++ 1_Sources/client-spotify/src/app/services/usuario.services.ts
@@ -0,0 +1,117 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+import { GLOBAL } from './global';
+import { Usuario } from '../models/usuario';
+
+@Injectable()
+export class UsuarioService {
+  public url: string;
+  public identity;
+  public token;
+
+  constructor(private _http: Http) {
+    this.url = GLOBAL.url;
+  }
+
+  signup(usuario_to_login, gethash = null) {
+    if (gethash) {
+      usuario_to_login.gethash = gethash;
+    }
+
+    const json = JSON.stringify(usuario_to_login);
+    const params = json;
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    return this._http.post(this.url + 'login', params, { headers: headers }).map(res => res.json());
+  }
+
+  getIdentity() {
+    let identity = JSON.parse(localStorage.getItem('identity'));
+
+    if (identity != 'undefined' && identity != null) {
+      this.identity = identity;
+    } else {
+      this.identity = null;
+    }
+
+    return this.identity;
+  }
+
+  getToken() {
+    let token = localStorage.getItem('token');
+
+    if (token != 'undefined' && token != null) {
+      this.token = token;
+    } else {
+      this.token = null;
+    }
+    return this.token;
+  }
+
+  //getEstructuraEmpresas() {
+  //  return this._http.get(this.url + 'estructuraempresas').map(res => res.json());
+  //}
+
+  //getEstructuraEmpresa(id: string) {
+  //  return this._http.get(this.url + 'estructuraempresa/' + id).map(res => res.json());
+  //}
+
+  //updateEstructuraEmpresa(id: string, estructuraempresa: EstructuraEmpresa) {
+  //  const json = JSON.stringify(estructuraempresa);
+  //  const params = json;
+  //  const headers = new Headers({ 'Content-Type': 'application/json' });
+
+  //  return this._http.put(this.url + 'estructuraempresa/' + id, params, { headers: headers }).map(res => res.json());
+  //}
+
+  //updateFather(id: string, estructuraempresa: EstructuraEmpresa) {
+  //  const json = JSON.stringify(estructuraempresa);
+  //  const params = json;
+  //  const headers = new Headers({ 'Content-Type': 'application/json' });
+
+  //  return this._http.put(this.url + 'estructuraempresafather/' + id, params, { headers: headers }).map(res => res.json());
+  //}
+
+  ///**
+  // * Añadir un elemento a la estructura de la empresa.
+  // */
+  //addEstructuraEmpresa(estructuraempresa: EstructuraEmpresa) {
+  //  const json = JSON.stringify(estructuraempresa);
+  //  const params = json;
+  //  const headers = new Headers({ 'Content-Type': 'application/json' });
+
+  //  return this._http.post(this.url + 'estructuraempresa', params, { headers: headers }).map(res => res.json());
+  //}
+
+  //getRelaciones(id: string) {
+  //  return this._http.get(this.url + 'relaciones/' + id).map(res => res.json());
+  //}
+
+  //delRelacion(id: string) {
+  //  const headers = new Headers({ 'Content-Type': 'application/json' });
+
+  //  return this._http.put(this.url + 'relacion/' + id, '', { headers: headers }).map(res => res.json());
+  //}
+
+  //getEmpleados() {
+  //  return this._http.get(this.url + 'empleados').map(res => res.json());
+  //}
+
+  //getRoles() {
+  //  return this._http.get(this.url + 'roles').map(res => res.json());
+  //}
+
+  ///**
+  // * Añadir un elemento a la estructura de la empresa.
+  // */
+  //addRecursos(id: string, recursos: any) {
+  //  const json = JSON.stringify(recursos);
+  //  const params = json;
+  //  const headers = new Headers({ 'Content-Type': 'application/json' });
+
+  //  return this._http.post(this.url + 'addrelacion/' + id, params, { headers: headers }).map(res => res.json());
+  //}
+}
